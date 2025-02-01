3256b53a7759222a0ab8fc034959fc6df67a90ee
DHRUVA K S <dhruvagowda460@gmail.com>
2025-02-01 12:08:01 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://52.66.205.57:9980/',
+  serverUrl: 'http://172.31.45.178:9980/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
